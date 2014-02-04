502c80d0bb4e8408bad08feb47b8b7d22d750a07
SammyNXS <doctor.hope@knights.ucf.edu>
2014-02-04 09:27:22 -0500

    Added Prefabs, set scene, and started created Controller for switching agents

---
+++ Assignment1_Sammy_Justin/Assets/AgentScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AgentScript : MonoBehaviour {
+	public double x;
+	public double y;
+	public double theta;
+
+	// Use this for initialization
+	public void Start () {
+		UpdatePosition();
+	}
+
+	// Update is called once per frame
+	public void Update () {
+		UpdatePosition();
+		Move();
+	}
+
+	private void UpdatePosition(){
+		x = transform.position.x;
+		y = transform.position.y;
+		theta = transform.eulerAngles.y;
+	}
+
+	private void Move(){
+//		int xAxis = ((Input.GetKey(right))?1:0)-((Input.GetKey(left))?1:0);
+//		xMove = xAxis*moveSpeed*Time.deltaTime;
+//		
+//		if((xMove != 0) && walkFlag){
+//			int multfactor = 1;
+//			int addFactor = 0;
+//			if(xMove < 0){
+//				multfactor = -1;
+//				addFactor = 1;
+//			}
+//			if(walk == 0){
+//				Debug.Log("stand");
+//				setOffset((standIndex+addFactor)*multfactor);
+//				++walk;
+//			}
+//			else if(walk == 1){
+//				Debug.Log("walk1");
+//				setOffset((standIndex+addFactor+1)*multfactor);
+//				++walk;
+//			}
+//			else if(walk == 2){
+//				Debug.Log("walk2");
+//				setOffset((standIndex+addFactor+2)*multfactor);
+//				++walk;
+//			}
+//			else if(walk == 3){
+//				Debug.Log("back21");				
+//				setOffset((standIndex+addFactor+1)*multfactor);
+//				walk = 0;
+//			}
+//			
+//			renderer.material.SetTextureOffset("_MainTex", offset);
+//			walkFlag=false;
+//			StartCoroutine("SpriteCool");
+//		}
+//		transform.
+//			transform.position+= new Vector3(xMove,0,0);
+	}
+}
---
+++ Assignment1_Sammy_Justin/Assets/ControllerScript.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerScript : MonoBehaviour {
+	
+	public KeyCode up = KeyCode.UpArrow;
+	public KeyCode down = KeyCode.DownArrow;
+	public KeyCode left = KeyCode.LeftArrow;
+	public KeyCode right = KeyCode.RightArrow;
+
+	public int agentNum = 0;
+
+	public GameObject currentAgent;
+
+	public GameObject[] agents; 
+
+	public bool selectMode = true;
+
+	public bool agentFlag = true;
+
+	// Use this for initialization
+	void Start () {
+		agents = GameObject.FindGameObjectsWithTag("Agent");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(agentFlag){
+			bool leftBool = Input.GetKey(left);
+			bool rightBool = Input.GetKey(right);
+
+			if(leftBool){
+				agentFlag = false;
+				StartCoroutine(AgentSelectCool(false));
+			}
+			else if(rightBool){
+				agentFlag = false;
+				StartCoroutine(AgentSelectCool(true));
+			}
+		}
+	}
+
+	// Use this for initializing GUI
+	void OnGUI () {
+		GUI.Box(new Rect(10,10,200,50),
+		        "Move arrows left and right to\n" +
+		        "selet an agent, then press enter\n" +
+		        "to control: Agent #" + agentNum);
+	}
+
+	IEnumerator AgentSelectCool(bool dir){
+		while(!agentFlag){
+			yield return new WaitForSeconds(1f/6);
+			// dir == true for right, false for left
+			if(dir){
+				if(agentNum == agents.Length -1){
+					agentNum = 0;
+				}
+				else{
+					++agentNum;
+				}
+			}
+			else{
+				if(agentNum == 0){
+					agentNum = agents.Length -1;
+				}
+				else{
+					--agentNum;
+				}
+			}
+			agentFlag = true;
+		}
+	}
+}
